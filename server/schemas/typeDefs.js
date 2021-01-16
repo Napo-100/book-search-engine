6d48b47d738236bb42a8eecfab6e27f0b6ec8086
Napo <Michaeljnapolitano@gmail.com>
2021-01-16 13:28:51 -0500

    set up typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,8 +1,28 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    savedBooks: [Book]
+  }
+
+  type Book {
+    _id: ID
+    authors: String
+    description: String
+    bookId: String
+    image: String
+    link: String
+    title: String
+  }
+
   type Query {
-    helloWorld: String
+    me: User
+    users: [User]
+    user(username: String): User
+    books(username: String): [Book]
   }
 `;
 
